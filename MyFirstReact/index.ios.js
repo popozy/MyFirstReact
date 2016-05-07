3ad993fc973bea282b040189cf0fe65714d06789
18868108662@163.com <18868108662@163.com>
2016-05-08 04:13:19 +0800

    give up make callback function a new animation function temporary

--- MyFirstReact/index.ios.js
+++ MyFirstReact/index.ios.js
@@ -26,8 +26,19 @@
     this.state = {
       fadeInOpacity: new Animated.Value(0),
       rotation: new Animated.Value(0),
-      // bounceValue: new Animated.Value(0)
+      // bounceValue: new Animated.Value(1.5),
+
+      //`try to callback another function to implement Animation after fisrt render finishing`
+      //`但是在该位置定义函数的时候this.state.bounceValue is undefined`
+      // AfterRender: new function(){
+      //   // this.state.bounceValue.setValue(1.5),
+      //   Animated.spring(this.state.bounceValue,{
+      //     toValue: 0.8,
+      //     friction: 1,
+      //   }).start();
+      // }
     };
+
   }
 
   render():ReactElement{
@@ -55,12 +66,16 @@
     }
 
     componentDidMount(){
+
+      //`first demo for opacity increasing with time`
       // Animated.timing(this.state.fadeInOpacity, {
       //   toValue: 1,
       //   duration: 1000,
-      //   // Easing: Easing.linear
+      //   // Easing: Easing.linear     //unsolved:error when running???
       // }).start();
       // this.state.bounceValue.setValue(1.5),
+
+      //`parallel animation`
       Animated.parallel(
         ['fadeInOpacity', 'rotation'].map(property =>{
           return Animated.timing(this.state[property],{
@@ -68,18 +83,24 @@
             duration: 2000
         })
 
-        //unsolved when wanna to use another Animated
+        //`unsolved when wanna to use another Animated`
         // Animated.spring(this.state.bounceValue,
         // {
         //   toValue: 0.8,
         //   friction: 1,
         // })
       })).start();
-
-      // Animated.parallel([
-      //   Animated.timing(this)
-      // ]).start();
     }
+
+      //`try to callback another function to implement Animation after fisrt render finishing`
+      //`但是在AfterRender位置出现unexpected token`
+      // AfterRender: new function(){
+      //   // this.state.bounceValue.setValue(1.5),
+      //   Animated.spring(this.state.bounceValue,{
+      //     toValue: 0.8,
+      //     friction: 1,
+      //   }).start();
+      // }
 }
 
   AppRegistry.registerComponent('MyFirstReact', () => MyFirstReact);
