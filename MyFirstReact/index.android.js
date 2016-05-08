bcc3d1d82ccead794496a7f47cde93e5da47c1af
18868108662@163.com <18868108662@163.com>
2016-05-09 00:11:25 +0800

    migrate ios demo to android demo

--- MyFirstReact/index.android.js
+++ MyFirstReact/index.android.js
@@ -1,51 +1,124 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ * @flow
  */
 
-import React, { Component } from 'react';
-import {
+'use strict';
+
+ var ReactNative = require('react-native');
+ import React, { Component } from 'react';
+ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  Animated,
+  PanResponder
 } from 'react-native';
 
-class MyFirstReact extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+
+
+ class MyFirstReact extends React.Component {
+  constructor(props: any){
+    super(props);
+    this.state = {
+      fadeInOpacity: new Animated.Value(0),
+      rotation: new Animated.Value(0),
+      bounceValue: new Animated.Value(1.5),
+
+      //`try to callback another function to implement Animation after fisrt render finishing`
+      //`但是在该位置定义函数的时候this.state.bounceValue is undefined`
+      // AfterRender: new function(){
+      //   // this.state.bounceValue.setValue(1.5),
+      //   Animated.spring(this.state.bounceValue,{
+      //     toValue: 0.8,
+      //     friction: 1,
+      //   }).start();
+      // }
+    };
+
   }
+
+  render():ReactElement{
+    return (<Animated.Image
+      source={require('./test.jpg')}
+      style = {{
+        opacity: this.state.fadeInOpacity,
+        transform:[
+        {
+          rotateZ: this.state.rotation.interpolate({
+            inputRange: [0,1],
+            outputRange: ['0deg', '360deg']
+          })
+        },
+        {scale: this.state.bounceValue}
+        ],
+        width:300,
+        height:300,
+        // flex: 1,        //unsolved problem: make it center at the view  by flex
+        // alignItems: 'center',
+        // justifyContent: 'center'
+        margin: 30
+      }}/>
+      );
+    }
+
+    componentDidMount(){
+
+      //`first demo for opacity increasing with time`
+      // Animated.timing(this.state.fadeInOpacity, {
+      //   toValue: 1,
+      //   duration: 1000,
+      //   // Easing: Easing.linear     //unsolved:error when running???
+      // }).start();
+      // this.state.bounceValue.setValue(1.5),
+
+      Animated.sequence(
+      [
+        //`parallel animation`
+        Animated.parallel(
+          // ['fadeInOpacity', 'rotation'].map(property =>{
+          //   return Animated.timing(this.state[property],{
+          //     toValue: 1,
+          //     duration: 2000
+          //   })
+
+        [ Animated.timing(this.state.fadeInOpacity, {
+            toValue: 1,
+            duration: 2000
+          }),
+           Animated.timing(this.state.rotation, {
+            toValue: 1,
+            duration: 2000
+           }),
+        ]
+          //`unsolved when wanna to use another Animated`
+          // Animated.spring(this.state.bounceValue,
+          // {
+          //   toValue: 0.8,
+          //   friction: 1,
+          // })
+        ),
+
+        // this.state.bounceValue.setValue(1.5),
+        Animated.spring(this.state.bounceValue,{
+          toValue: 0.8,
+          friction: 1,
+        })
+      ]).start();
+    }
+
+      //`try to callback another function to implement Animation after fisrt render finishing`
+      //`但是在AfterRender位置出现unexpected token`
+      // AfterRender: new function(){
+      //   // this.state.bounceValue.setValue(1.5),
+      //   Animated.spring(this.state.bounceValue,{
+      //     toValue: 0.8,
+      //     friction: 1,
+      //   }).start(;
+      // }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('MyFirstReact', () => MyFirstReact);
+  AppRegistry.registerComponent('MyFirstReact', () => MyFirstReact);
